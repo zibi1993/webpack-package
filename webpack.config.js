7d9790d499490164eb7ae3b4ffa1a8c0bab75fa7
zibi1993 <s.zygmunt1993@gmail.com>
2019-10-08 14:14:14 +0200

    installing babel && create babelrc

--- webpack.config.js
+++ webpack.config.js
@@ -14,8 +14,8 @@ module.exports = {
     module: {
         rules:[
             {
-                test:/\.js$/,
-                loader:,
+                test: /\.js$/,
+                loader: 'babel-loader',
                 exclude: /node_module/
             }
         ]
